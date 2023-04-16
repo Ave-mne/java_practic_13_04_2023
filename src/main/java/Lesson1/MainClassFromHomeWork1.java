f6f92112d2014bca170af20608b3945cdff8c15c
«Valeev_Andrey» <Valeevptk@yandex.ru>
2023-04-16 18:50:14 +0300

    Решена 3 задача

--- src/main/java/Lesson1/MainClassFromHomeWork1.java
+++ src/main/java/Lesson1/MainClassFromHomeWork1.java
@@ -6,11 +6,12 @@ import java.util.Scanner;
 public class MainClassFromHomeWork1 {
     public static void main(String[] args) {
         //ex1();
-        ex2();
+        //ex2();
         //ex3();
         //ex4();
         //ex5();
     }
+
 
 
 
@@ -62,9 +63,23 @@ private static void ex1() {
         System.out.println("max = "+max+" min = "+min);
     }
 
+// 3. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+// и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из
+// диагоналей, если обе сложно). Определить элементы одной из диагоналей можно по следующему
+// принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
 
-
-
+    private static void ex3(int[][] arr)
+    {
+        for (int i = 0; i <= arr.length; i++) {
+            for (int j = 0; j <= arr.length; j++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+    }
 
 
 }
